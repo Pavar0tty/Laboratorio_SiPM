884c792f4cf3ac36c16a35eb4f136ab3e8916a22
TeoCoz <matteocoz11@gamil.com>
2025-05-29 10:05:51 +0200

    Funzione di conversione adc in energia

--- Config/lablib.py
+++ Config/lablib.py
@@ -174,3 +174,9 @@ def gauss_pol3 (x, mu, sigma, S, a, b, c, d):
 
 def gauss_exp(x, mu, sigma, S, a, lam):
     return gauss(x, mu, sigma, S) + a * np.exp(-lam*x) 
+
+def conv_ADC_Energy(C):
+    """
+    Prende in entrata il numero di ADC chennels e restituisce l'energia in eV.
+    """
+    return (2/50)*(1/2**12)*(4e-9)*1/25*C/1.602176634e-19*3.6
